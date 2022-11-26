0763f807de79cd7add5145847209896c80045f24
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-11-26 16:34:14 +0100

    Refactor

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,8 +14,22 @@ module "celebrator-3000" {
   image_url = "docker.io/tomondre/celebrator-3000"
   name      = "celebrator-3000"
   image_tag = var.image_tag
-  env       = var.env
+  env       = {
+    BASIN_HOST = var.basin_host
+    DATABASE_URL= var.database_url
+    RABBITMQ_USERNAME = var.rabbitmq_username
+    RABBITMQ_PASSWORD = var.rabbitmq_password
+    RABBITMQ_HOST = var.rabbitmq_host
+    RABBITMQ_PORT = var.rabbitmq_port
+    EMAILS = var.emails
+  }
 }
 
+variable "basin_host" {}
+variable "database_url" {}
+variable "rabbitmq_username" {}
+variable "rabbitmq_password" {}
+variable "rabbitmq_host" {}
+variable "rabbitmq_port" {}
+variable "emails" {}
 variable "image_tag" {}
-variable "env" {}
